1ae07733d90fb9a4f56c1179bb3daf111477f308
Ing. David Fiedler <fiedlda1@fel.cvut.cz>
2017-05-04 14:19:04 +0200

    Current scenario - workers moves vehicles to parking lotajavadoc, refactoring

--- maven-plugin/src/main/java/ninja/fido/config/plugin/Plugin.java
+++ maven-plugin/src/main/java/ninja/fido/config/plugin/Plugin.java
@@ -10,49 +10,38 @@ import org.apache.maven.plugins.annotations.Parameter;
 import org.apache.maven.project.MavenProject;
  
 /**
- * Says "Hi" to the user.
- *
+ * FutureConfig plugin. It simply run a build config procedure in your project's root package directory using 
+ * the config file suplied in the path parametr.
  */
 @Mojo(name = "build-config")
 public class Plugin extends AbstractMojo
 {
 	
+    /**
+     * Absolute path to the config file.
+     */
 	@Parameter(property = "build-config.path")
 	private String path;
-	
-//	@Parameter(property = "project.build.sourceDirectory")
-//    private File outputDirectory;
-	
+
+	/**
+     * Maven project.
+     */
 	@Parameter(defaultValue = "${project}", required = true, readonly = true)
-	MavenProject project;
+	private MavenProject project;
 	
+    
+    
+    
+    
 	@Override
     public void execute() throws MojoExecutionException
     {
-        getLog().info( "Hello, world." + path);
 		File configFile = new File(path);
-		MavenProject project = (MavenProject) getPluginContext().get("project");
-		
-		List roots = project.getCompileSourceRoots();
-		getLog().info( project.getBasedir().getAbsolutePath());
-//		new ConfigBuilder(file).buildConfig();
-//		String mainPackagePath = getMainPackagePath();
 		String srcPath = (String) project.getCompileSourceRoots().get(0);
 		String mainPackageName = getMainPackageName();
 		
 		new ConfigBuilder(configFile, new File(srcPath), mainPackageName + ".config").buildConfig();
     }
-
-	private String getMainPackagePath() {
-		String srcDirPath = (String) project.getCompileSourceRoots().get(0);
-		String packageDirPath = getDirPathFromGroupId();
-		String finalDirName = project.getArtifact().getArtifactId();
-		return srcDirPath + File.separator + packageDirPath + File.separator + finalDirName;
-	}
-
-	private String getDirPathFromGroupId() {
-		return project.getArtifact().getGroupId().replace('.', File.separatorChar);
-	}
 
 	private String getMainPackageName() {
 		return project.getArtifact().getGroupId() + "." + project.getArtifact().getArtifactId();
