9c086ac48eaec808025c6f6ea98090d74dc3206b
Fido <david.fido.fiedler@gmail.com>
2017-10-30 19:28:12 +0100

    license

--- maven-plugin/src/main/java/ninja/fido/config/plugin/PluginTools.java
+++ maven-plugin/src/main/java/ninja/fido/config/plugin/PluginTools.java
@@ -1,5 +1,5 @@
-/*
- * Copyright 2017 David Fiedler.
+/* 
+ * Copyright 2017 fido.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
