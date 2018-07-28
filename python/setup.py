ba6cad6c7bb32f8068db1f533bd5a490431b5288
Ing. David Fiedler <fiedlda1@fel.cvut.cz>
2018-07-28 18:08:47 +0200

    packiging files,

--- python/setup.py
+++ python/setup.py
@@ -1,13 +1,20 @@
+import setuptools
 from setuptools import setup
 
+# with open("README.rst", "r") as fh:
+#     long_description = fh.read()
+
 setup(
-   name='fconfig',
-   version='1.0',
-   description='Future Config',
-   author='F.I.D.O.',
-   author_email='david.fido.fiedler@gmail.com',
-   license='MIT',
-   packages=['fconfig'],
-   url = 'https://github.com/F-I-D-O/Future-Config/',
-   install_requires=['Mako==1.0.6', 'setuptools==33.1.1', 'typing==3.6.2'],
+	name='fconfig',
+	version='1.0',
+	description='Python config loader for Future Config format.',
+	author='F.I.D.O.',
+	author_email='david.fido.fiedler@gmail.com',
+	# long_description=long_description,
+	# long_description_content_type="text/markdown",
+	license='MIT',
+	packages=setuptools.find_packages(),
+	url = 'https://github.com/F-I-D-O/Future-Config/',
+	install_requires=['Mako', 'setuptools', 'typing'],
+	python_requires='>=3'
 )
