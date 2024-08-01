1bea5781b9c60c76d298b0378a812f00107867c4
David Fiedler <david.fido.fiedler@gmail.com>
2024-08-01 20:42:23 +0200

    vcpkg integration almost finished: publication missing

---
+++ cpp/install/builderRunner.cmake
@@ -0,0 +1,54 @@
+
+function(run_fconfig_builder)
+	cmake_parse_arguments(
+		PARSE_ARGV
+		0
+		RUN_FCONFIG_BUILDER
+		""
+		"MAIN_CONFIG_PATH;ROOT_CONFIG_CLASS_NAME;SOURCE_DIR"
+		""
+	)
+
+	# by default, we look for the main config file in the CMakeLists.txt directory
+	if(NOT DEFINED RUN_FCONFIG_BUILDER_MAIN_CONFIG_PATH)
+		set(RUN_FCONFIG_BUILDER_MAIN_CONFIG_PATH "${CMAKE_SOURCE_DIR}/config.yaml")
+	endif()
+
+	# by default, we set the root config class name to the name of the project
+	if(NOT DEFINED RUN_FCONFIG_BUILDER_ROOT_CONFIG_CLASS_NAME)
+		get_filename_component(RUN_FCONFIG_BUILDER_ROOT_CONFIG_CLASS_NAME "${CMAKE_PROJECT_NAME}" NAME)
+	endif()
+
+	# by default, we set the <CmakeLists.txt directory>/src as the source directory
+	if(NOT DEFINED RUN_FCONFIG_BUILDER_SOURCE_DIR)
+		set(RUN_FCONFIG_BUILDER_SOURCE_DIR "${CMAKE_SOURCE_DIR}/src")
+	endif()
+
+	set(FCONFIG_BUILDER_DIR "${VCPKG_INSTALLED_DIR}/${VCPKG_TARGET_TRIPLET}/tools/fconfig")
+	set(FCONFIG_BUILDER_EXECUTABLE "${FCONFIG_BUILDER_DIR}/fconfig_builder")
+	set(FCONFIG_BUILDER_ARGS
+		--main "${RUN_FCONFIG_BUILDER_MAIN_CONFIG_PATH}"
+		--name ${RUN_FCONFIG_BUILDER_ROOT_CONFIG_CLASS_NAME}
+		--source_dir "${RUN_FCONFIG_BUILDER_SOURCE_DIR}"
+	)
+	# change directory and run the fconfig_builder
+	execute_process(
+		COMMAND ${CMAKE_COMMAND} -E
+		chdir ${FCONFIG_BUILDER_DIR}
+		${FCONFIG_BUILDER_EXECUTABLE}
+		${FCONFIG_BUILDER_ARGS}
+		RESULT_VARIABLE FCONFIG_BUILDER_RESULT
+		OUTPUT_VARIABLE FCONFIG_BUILDER_OUTPUT
+		ERROR_VARIABLE FCONFIG_BUILDER_OUTPUT
+	)
+	if(FCONFIG_BUILDER_RESULT)
+		message(
+			FATAL_ERROR
+			"fconfig_builder failed with code ${FCONFIG_BUILDER_RESULT}.
+		The path to the fconfig_builder executable is: ${FCONFIG_BUILDER_EXECUTABLE}
+		The output was:\n${FCONFIG_BUILDER_OUTPUT}"
+		)
+	else()
+		message(STATUS "Configuration classes generated successfully")
+	endif()
+endfunction()
--- cpp/port/fconfig/portfile.cmake
+++ cpp/port/fconfig/portfile.cmake
@@ -7,8 +7,8 @@
 #	REF master
 #	SHA512 ab8cc92b3f0b2344c8e22f6d9522f64dd00c556f1cd5a8a139a1981d227bc371550de695c0a1ccd93e13526c9a74e69a0c8927faf5436f965147a822354a6fa7
 #)
-#set(SOURCE_PATH "D:/Workspaces/Fido/Future-Config")
-set(SOURCE_PATH "/mnt/d/Workspaces/Fido/Future-Config")
+set(SOURCE_PATH "D:/Workspaces/Fido/Future-Config")
+#set(SOURCE_PATH "/mnt/d/Workspaces/Fido/Future-Config")
 #set(SOURCE_PATH "C:/Workspaces/ninja/Future-Config")
 
 set(PACKAGE_NAME "fconfig")
