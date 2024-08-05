99f4c2b399ff984c7e5cdf4adbf4fb0436664ed6
David Fiedler <david.fido.fiedler@gmail.com>
2024-08-06 00:06:43 +0200

    static and shared library variants; some linking fixes;

--- cpp/port/future-config/portfile.cmake
+++ cpp/port/future-config/portfile.cmake
@@ -1,17 +1,20 @@
-vcpkg_from_github(
-	OUT_SOURCE_PATH SOURCE_PATH
-	REPO F-I-D-O/Future-Config
-	REF master
-	SHA512 0
-)
-#set(SOURCE_PATH "D:/Workspaces/Fido/Future-Config")
+#vcpkg_from_github(
+#	OUT_SOURCE_PATH SOURCE_PATH
+#	REPO F-I-D-O/Future-Config
+#	REF master
+#	SHA512 0
+#)
+set(SOURCE_PATH "D:/Workspaces/Fido/Future-Config")
 #set(SOURCE_PATH "/mnt/d/Workspaces/Fido/Future-Config")
 #set(SOURCE_PATH "C:/Workspaces/ninja/Future-Config")
 
 set(PACKAGE_NAME "future-config")
 
+string(COMPARE EQUAL "${VCPKG_LIBRARY_LINKAGE}" "dynamic" BUILD_SHARED)
+
 vcpkg_cmake_configure(
 	SOURCE_PATH ${SOURCE_PATH}/cpp
+	OPTIONS -DFCONFIG_BUILD_SHARED_LIBS=${BUILD_SHARED}
 )
 
 vcpkg_cmake_install()
@@ -25,11 +28,17 @@ vcpkg_copy_tools(
 )
 
 # move the jinja template for the builder tool from bin directory to tools directory
-set(BIN_DATA_DIR "${CURRENT_PACKAGES_DIR}/bin/data")
+set(BIN_DIR "${CURRENT_PACKAGES_DIR}/bin")
+set(BIN_DATA_DIR "${BIN_DIR}/data")
 set(PORT_TOOL_DATA_DIR "${CURRENT_PACKAGES_DIR}/tools/${PORT}/data")
 file(MAKE_DIRECTORY "${PORT_TOOL_DATA_DIR}")
 file(RENAME "${BIN_DATA_DIR}/config.jinja" "${PORT_TOOL_DATA_DIR}/config.jinja")
-file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/bin")
+file(REMOVE_RECURSE "${BIN_DATA_DIR}")
+# also delete the bin directory if it is empty
+file(GLOB dir_to_rm_content "${BIN_DIR}/*")
+if("${dir_to_rm_content}" STREQUAL "")
+	file(REMOVE_RECURSIVE "${BIN_DIR}")
+endif()
 
 file(INSTALL "${SOURCE_PATH}/LICENSE.txt" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}" RENAME copyright)
 
