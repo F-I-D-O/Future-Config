80480a0f0423de50c92b4d72c1f4832c5983c4f1
F.I.D.O. <david.fido.fiedler@gmail.com>
2025-11-06 23:10:23 -0500

    path in mamba install fixed

--- cpp/test/vcpkg_gcc11/install_gcc-11-mamba.sh
+++ cpp/test/vcpkg_gcc11/install_gcc-11-mamba.sh
@@ -5,14 +5,14 @@ dnf install mamba -y
 mamba create -n gcc11 -c conda-forge gcc_linux-64=11 gxx_linux-64=11 -y
 ENV="/envs/gcc11"
 
-tee /usr/local/bin/gcc-11 >/dev/null <<SH
+tee /usr/bin/gcc-11 >/dev/null <<SH
 #!/usr/bin/env bash
 exec "$ENV/bin/x86_64-conda-linux-gnu-gcc" "\$@"
 SH
-sudo chmod +x /usr/local/bin/gcc-11
+sudo chmod +x /usr/bin/gcc-11
 
-tee /usr/local/bin/g++-11 >/dev/null <<SH
+tee /usr/bin/g++-11 >/dev/null <<SH
 #!/usr/bin/env bash
 exec "$ENV/bin/x86_64-conda-linux-gnu-g++" "\$@"
 SH
-sudo chmod +x /usr/local/bin/g++-11
+sudo chmod +x /usr/bin/g++-11
